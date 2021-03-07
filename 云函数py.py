561ab6329b27b149c4aa028de63519a5dc4e32d5
Frank <43460484+FrankYang3110@users.noreply.github.com>
2021-03-07 15:10:21 +0800

    Update 云函数py.py

--- 云函数py.py
+++ 云函数py.py
@@ -4,7 +4,7 @@ sever = 'on'
 # 填写server酱sckey,不开启server酱则不用填（自己更改）
 sckey = 'SCU163905T95412fda7d2e8f6f77c039d80c795151604478ae36dc4'
 # 填入glados账号对应cookie
-cookie = '__cfduid=d968be8b32a4871e5b7f06889137f095f1615083756; koa:sess=eyJ1c2VySWQiOjQ2Nzg5LCJfZXhwaXJlIjoxNjQxMDAzOTM2MDY0LCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=yilXSnREn_cSoafFFyDX--TJIs0; _ga=GA1.2.933335283.1615085040; _gid=GA1.2.244699911.1615085040'
+cookie = '__cfduid=d968be8b32a4871e5b7f06889137f095f1615083756; koa:sess=eyJ1c2VySWQiOjQ2Nzg5LCJfZXhwaXJlIjoxNjQxMDAzOTM2MDY0LCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=yilXSnREn_cSoafFFyDX--TJIs0; _ga=GA1.2.933335283.1615085040; _gid=GA1.2.244699911.1615085040; _gat_gtag_UA_104464600_2=1'
 referer = 'https://glados.rocks/console/checkin'
 
 def start():
